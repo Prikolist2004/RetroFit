ae2295167da8bde4fd3ef2bde3d01c48e0fda476
Prikolist2004 <Aytist39@gmail.com>
2024-10-09 14:39:19 +0900

    v2

---
+++ app/src/main/java/com/example/retrofitlesson/retrofit/User.kt
@@ -0,0 +1,14 @@
+package com.example.retrofitlesson.retrofit
+
+import android.media.Image
+
+data class User(
+    val id: Int,
+    val username: String,
+    val email: String,
+    val firstName: String,
+    val lastName: String,
+    val gender: String,
+    val image: String,
+    val token: String
+)
